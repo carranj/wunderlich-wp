1ff55fe75917c86da061591a494e1fd4dbfb114f
carranj <carranco.jose.r@gmail.com>
2020-07-21 23:54:17 -0500

    function navbar

--- wp-content/themes/wis/header.php
+++ wp-content/themes/wis/header.php
@@ -16,7 +16,7 @@
   <header>
     <div class="container">
       <div class="logo-container">
-        logo area
+        <a href="<?php echo site_url('/'); ?>"><img src="http://localhost/wis/wp-content/uploads/2020/07/logo.png" alt=""></a>
       </div>
       <nav>
         <?php
