77be346cb5f93ed51570deb88274bae392b60e10
carranj <carranco.jose.r@gmail.com>
2020-07-24 12:54:09 -0500

    update home page with acf

--- wp-content/themes/wis/front-page.php
+++ wp-content/themes/wis/front-page.php
@@ -2,12 +2,8 @@
 <main>
     <section class="home-intro" style="background: url(http://josecarranco.com/wis/wp-content/uploads/2020/07/intro-photo.png);">
         <div class='home-jumbotron col-md-12'>
-            <h1>Welcome to Wunderlich Band</h1>
-            <p>1st Division UIL Varsity Band Stage Performance</p>
-            <p>1st Division Full Orchestra UIL</p>
-            <p>Full Orchestra placed 10th in the state (2nd top 10 finish in past two years)</p>
-            <p>1st Division UIL Non Varsity for Stage Performance and Sight Reading</p>
-            <p>1st Division Varsity Band at Director's Choice Music Festivals</p>
+            <h1><?php the_field('intro_title'); ?></h1>
+            <?php the_field('intro_paragraph'); ?>
         </div>
     </section>
     
